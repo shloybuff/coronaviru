0bf8a57703e3d50da2cfe2cf734f56c41cf2075b
chloy <buffshloy55@gmail.com>
2020-06-21 12:40:08 +0200

    Updates

---
+++ precache-manifest.cfa2c349d49a049d7dc1e36be46b6303.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7abcae70896f9e8408006385a0632083",
+    "url": "/coronaviru/index.html"
+  },
+  {
+    "revision": "5c144f3fa5ef951fe541",
+    "url": "/coronaviru/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "56a2be20a4eeaeeeb76b",
+    "url": "/coronaviru/static/css/main.597ec5a7.chunk.css"
+  },
+  {
+    "revision": "5c144f3fa5ef951fe541",
+    "url": "/coronaviru/static/js/2.4ef02802.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/coronaviru/static/js/2.4ef02802.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56a2be20a4eeaeeeb76b",
+    "url": "/coronaviru/static/js/main.9dac5faa.chunk.js"
+  },
+  {
+    "revision": "fd9baa658dae69d078c4",
+    "url": "/coronaviru/static/js/runtime-main.227b797d.js"
+  }
+]);
